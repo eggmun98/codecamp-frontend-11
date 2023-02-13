d296a0c7539a74d04f7061aaba27305132f6d260
eggmun98 <eggmun98@gmail.com>
2023-02-13 18:29:21 +0900

    로그인 공부 정리

---
+++ freeboard_frontend/src/commons/libraries/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBLxL-EE5cBzfzXtgt8FrATSbLk2lL7oW8",
+  authDomain: "notice-587ce.firebaseapp.com",
+  projectId: "notice-587ce",
+  storageBucket: "notice-587ce.appspot.com",
+  messagingSenderId: "854481898254",
+  appId: "1:854481898254:web:8d75546ebe6d6f2d4fa55f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
